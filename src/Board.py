7b6abb1e61007e6b39976ec12104d90da019e96c
Andrean Lay <andreanlay1@gmail.com>
2020-01-13 22:10:56 +0700

    Revert "Change generating method"
    This reverts commit 8d45f38919dd8d31577b75fc39fd91ad450646df.

--- src/Board.py
+++ src/Board.py
@@ -1,29 +1,18 @@
 from random import randint
 
-
 class Board:
     def __init__(self):
         self.board = [[0] * 9 for _ in range(9)]
         self.generate_board()
 
     def generate_board(self):
-        self.fill_diagonal_boxes()
-        self.fill_remaining_boxes()
+        self.fill_all_boxes()
 
-    def fill_diagonal_boxes(self):
-        for i in range(0, 9, 3):
-            for k in range(3):
-                for l in range(3):
-                    num = randint(1, 9)
-                    while self.innerbox_is_safe(i, i, num) is False:
-                        num = randint(1, 9)
-                    self.board[i+k][i+l] = num
-
-    def fill_remaining_boxes(self):
+    def fill_all_boxes(self):
         flag = [False for _ in range(9)]
         empty = self.find_empty()
-
-        # If no empty box, finish generating
+        
+        #If no empty box, finish generating
         if empty is None:
             return True
 
@@ -33,10 +22,10 @@ class Board:
             flag[num - 1] = True
             if self.safe_to_fill(row, col, num):
                 self.board[row][col] = num
-                if self.fill_remaining_boxes():
+                if self.fill_all_boxes():
                     return True
                 self.board[row][col] = 0
-
+                
             # If none of numbers fit, backtrack to previous block
             if flag.count(True) == 9:
                 return False
@@ -54,7 +43,7 @@ class Board:
             if self.board[i][col] == num:
                 return False
         return True
-
+  
     # Check 3x3 box whether a number is used
     def innerbox_is_safe(self, row, col, num):
         for i in range(3):
@@ -65,8 +54,8 @@ class Board:
 
     def safe_to_fill(self, row, col, num):
         return self.row_is_safe(row, num) and self.col_is_safe(col, num) and self.innerbox_is_safe(row - row % 3,
-                                                                                                   col - col % 3,
-                                                                                                   num)
+                                                                                                      col - col % 3,
+                                                                                                      num)
 
     def find_empty(self):
         for i in range(9):
